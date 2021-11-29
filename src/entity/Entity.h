34fec2ecfef211979a794e6522b02073d0fbcbaa
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 15:15:12 -0300

    Configurado de libreria SFML

---
+++ src/entity/Entity.h
@@ -0,0 +1,28 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+
+struct Vector2f
+{
+	float x;
+	float y;
+};
+
+class Entity
+{
+public:
+	Entity(Vector2f position);
+	virtual ~Entity();
+
+	virtual void update() = 0;
+	virtual void draw();
+	virtual void collisionWith(Entity* entity);
+
+private:
+	Vector2f position;
+
+
+};
+
+
+#endif // !ENTITY_H
