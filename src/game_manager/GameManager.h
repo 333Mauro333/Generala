6fe3dc2f43d7d80be0ac5a5694e5d39a98893804
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 21:43:05 -0300

    Agregado de clase GameManager.h

---
+++ src/game_manager/GameManager.h
@@ -0,0 +1,21 @@
+#ifndef GAME_MANAGER_H
+#define GAME_MANAGER_H
+
+
+class GameManager
+{
+private:
+
+
+public:
+	GameManager();
+	~GameManager();
+
+	void init();
+	void update();
+	void draw();
+	void destroy();
+};
+
+
+#endif // !GAME_MANAGER_H
