324447d228bf7817ef7ed94ebe4cacd05588d570
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 22:14:27 -0300

    Mejorado del codigo de la clase de GameManager

--- src/game_manager/GameManager.cpp
+++ src/game_manager/GameManager.cpp
@@ -1,29 +1,60 @@
 #include "GameManager.h"
 
+#include <iostream>
 
-GameManager::GameManager()
+using std::cout;
+
+
+GameManager::GameManager(unsigned int width, unsigned int height, const std::string windowTitle)
 {
-
+	window.create(sf::VideoMode(width, height), windowTitle);
+    
+	cout << "Se ha creado un GameManager.\n";
 }
 GameManager::~GameManager()
 {
-
+	cout << "El GameManager ha sido eliminado de la memoria.\n\n";
 }
 
 void GameManager::init()
 {
-
+    // Se llama al init de la escena.
 }
-void GameManager::update()
+void GameManager::update(float deltaTime)
 {
+    init();
 
+    while (window.isOpen())
+    {
+        checkEvents();
+
+        // Obtener la escena actual para hacer el update.
+        
+        draw();
+    }
+
+    destroy();
 }
 void GameManager::draw()
 {
+    window.clear();
 
+    // Se dibuja lo que tenga la escena.
+
+    window.display();
 }
 void GameManager::destroy()
 {
-
+    // Se 
 }
 
+
+void GameManager::checkEvents()
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window.close();
+    }
+}
--- src/game_manager/GameManager.h
+++ src/game_manager/GameManager.h
@@ -1,18 +1,25 @@
 #ifndef GAME_MANAGER_H
 #define GAME_MANAGER_H
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+using sf::RenderWindow;
+
 
 class GameManager
 {
 private:
+	RenderWindow window;
 
+	void checkEvents();
 
 public:
-	GameManager();
+	GameManager(unsigned int width, unsigned int height, const std::string windowTitle);
 	~GameManager();
 
 	void init();
-	void update();
+	void update(float deltaTime);
 	void draw();
 	void destroy();
 };
