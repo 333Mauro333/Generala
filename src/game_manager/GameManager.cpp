ec79e3ae4afe223a91b2a07266c84aa0005e227f
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 21:45:36 -0300

    Agregado de clase GameManager.cpp

---
+++ src/game_manager/GameManager.cpp
@@ -0,0 +1,29 @@
+#include "GameManager.h"
+
+
+GameManager::GameManager()
+{
+
+}
+GameManager::~GameManager()
+{
+
+}
+
+void GameManager::init()
+{
+
+}
+void GameManager::update()
+{
+
+}
+void GameManager::draw()
+{
+
+}
+void GameManager::destroy()
+{
+
+}
+
