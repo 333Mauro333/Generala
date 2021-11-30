ce1018904e1f27cc41c62233e25503a24fc2723a
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 22:22:35 -0300

    Agregado de clase main_menu

---
+++ src/scenes/main_menu/main_menu.cpp
@@ -0,0 +1,12 @@
+#include "main_menu.h"
+
+
+MainMenu::MainMenu()
+{
+
+}
+MainMenu::~MainMenu()
+{
+
+}
+
---
+++ src/scenes/main_menu/main_menu.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+class MainMenu
+{
+private:
+
+
+public:
+	MainMenu();
+	~MainMenu();
+
+
+};
+
+
+#endif // !MAIN_MENU_H
+
