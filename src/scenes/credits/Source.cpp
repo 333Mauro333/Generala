f8b82409b48671a039a5b904f7a2ee2958fe7028
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 22:38:51 -0300

    Agregado de la clase Credits

---
+++ src/scenes/credits/Source.cpp
@@ -0,0 +1,28 @@
+#include "credits.h"
+
+
+Credits::Credits()
+{
+
+}
+Credits::~Credits()
+{
+
+}
+
+void Credits::init()
+{
+
+}
+void Credits::update(float deltaTime)
+{
+
+}
+void Credits::draw(RenderWindow window)
+{
+
+}
+void Credits::destroy()
+{
+
+}
---
+++ src/scenes/credits/credits.h
@@ -0,0 +1,22 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+#include "scenes/scene.h"
+
+
+class Credits : Scene
+{
+private:
+
+
+public:
+	Credits();
+	~Credits();
+
+	void init() override;
+	void update(float deltaTime) override;
+	void draw(RenderWindow window) override;
+	void destroy() override;
+};
+
+#endif // !CREDITS_H
