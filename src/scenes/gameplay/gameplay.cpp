4c2163e98ffb48489ab3cef87445cfc7ca65503a
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-11-29 22:50:08 -0300

    Agregado de la clase de gameplay

---
+++ src/scenes/gameplay/gameplay.cpp
@@ -0,0 +1,32 @@
+#include "gameplay.h"
+
+#include <iostream>
+
+using std::cout;
+
+
+Gameplay::Gameplay()
+{
+	cout << "Se ha creado una pantalla de gameplay.\n";
+}
+Gameplay::~Gameplay()
+{
+	cout << "La pantalla de gameplay ha sido eliminada de la memoria.\n";
+}
+
+void Gameplay::init()
+{
+
+}
+void Gameplay::update(float deltaTime)
+{
+
+}
+void Gameplay::draw(RenderWindow window)
+{
+
+}
+void Gameplay::destroy()
+{
+
+}
---
+++ src/scenes/gameplay/gameplay.h
@@ -0,0 +1,22 @@
+#ifndef GAMEPLAY_H
+#define GAMEPLAY_H
+
+#include "scenes/scene.h"
+
+
+class Gameplay : Scene
+{
+private:
+
+public:
+	Gameplay();
+	~Gameplay();
+
+	void init() override;
+	void update(float deltaTime) override;
+	void draw(RenderWindow window) override;
+	void destroy() override;
+
+};
+
+#endif // !GAMEPLAY_H
